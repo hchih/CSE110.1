6561e13d635d8cda010ca30428d7402479205e93
Ann Chih <hchih@ucsd.edu>
2015-11-08 09:52:50 -0800

    add first page

--- AndroidStudioProjects/110-master/app/src/main/java/com/example/annchih/classplanner/MainActivity.java
+++ AndroidStudioProjects/110-master/app/src/main/java/com/example/annchih/classplanner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.annchih.classplanner;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,66 +9,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.annchih.classplanner.dummy.Calender_Activity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
-    private Spinner college,major,year;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-        //Spinner spinner = (Spinner)findViewById(R.id.college_spinner);
-        //ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.college_spinner, R.layout.spinner_style);
-        //spinner.setAdapter();
+        Button toMain = (Button)findViewById(R.id.finishFirstStep);
+        toMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,QuarterListView.class);
+                startActivity(intent);
+            }
+        });
     }
-
-    /*@Override
-    public void selectClass(View view){
-        //Intent intent = new Intent(this,ClassSelect.class);
-    }*/
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void classesTaken(View view) {
-        Intent intent = new Intent(this, ItemListActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.edit_message);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-    
-    public void calenderPlanner(View view) {
-        Intent intent = new Intent(this, Calender_Activity.class);
-//        EditText editText = (EditText) findViewById(R.id.edit_message);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-
 
 }
